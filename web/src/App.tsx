1001966f3943c4ccb49f57694f9b9b3cb37f0846
Andrei Gramescu <36415459+andreigramescu@users.noreply.github.com>
2021-10-15 15:46:34 +0100

    web interface: add demo for the gpu view

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,40 +1,24 @@
-import { useRef, useState } from 'react'
-import { Canvas, useFrame, ReactThreeFiber } from '@react-three/fiber'
-
-function Box(props: {position: number[]}) {
-  const mesh = useRef<ReactThreeFiber.MeshProps>()
-
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-
-  const rot = Math.random();
-  useFrame(() => (mesh.current!.rotation.x += rot))
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 2.5 : 1}
-      onClick={() => setActive(!active)}
-      onPointerOver={() => setHover(true)}
-      onPointerOut={() => setHover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
+import { useState } from 'react'
+import GPUView from "./gpu/GPUView"
 
 function App() {
+  // These are here just for the demo. Will be removed
+  const [w, setW] = useState(800);
+  const [h, setH] = useState(800);
+  (window as any).funkyFunc = (x: number, y: number) => { setW(x); setH(y) }
+
+  const cssCenter = {
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
+  } as const
+
   return (
-    <div className="App" style={{textAlign: "center"}}>
-      <h1>Hover over / click on one of the cubes</h1>
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-3.2, 0, 0]} />
-        <Box position={[3.2, 0, 0]} />
-      </Canvas>
+    <div className="App" style={cssCenter}>
+      <h1>The view should resize by call of 'funkyFunc'</h1>
+      <GPUView height={h} width={w}/>
     </div>
   );
 }
