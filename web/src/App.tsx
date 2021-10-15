82c0947a8f0bb0e51a4f2062fb25cc49c7b2631f
Andrei Gramescu <36415459+andreigramescu@users.noreply.github.com>
2021-10-15 17:05:44 +0100

    web interface: add tsx formatting config and script

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,29 @@
-import { useState } from 'react'
+import { useState } from "react"
 import GPUView from "./gpu/GPUView"
 
 function App() {
   // These are here just for the demo. Will be removed
-  const [w, setW] = useState(800);
-  const [h, setH] = useState(800);
-  (window as any).funkyFunc = (x: number, y: number) => { setW(x); setH(y) }
+  const [w, setW] = useState(800)
+  const [h, setH] = useState(800)
+  ;(window as any).funkyFunc = (x: number, y: number) => {
+    setW(x)
+    setH(y)
+  }
 
   const cssCenter = {
     textAlign: "center",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   } as const
 
   return (
     <div className="App" style={cssCenter}>
       <h1>The view should resize by call of 'funkyFunc'</h1>
-      <GPUView height={h} width={w}/>
+      <GPUView height={h} width={w} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
