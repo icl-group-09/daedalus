c2264bbc9249166188f7e46a680498d600116bd9
Andrei Gramescu <36415459+andreigramescu@users.noreply.github.com>
2021-10-15 15:53:58 +0100

    web interface: fix broken indentation

--- web/src/gpu/GPUView.tsx
+++ web/src/gpu/GPUView.tsx
@@ -8,37 +8,36 @@ function init(domElem: HTMLElement, width: number, height: number) {
   const renderer = new THREE.WebGLRenderer({ antialias: true })
   renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setSize(width, height)
-
+  
   // Mount the GPU view to the HTML 
   const children = domElem.children
   if (children.length > 0) {
     domElem.removeChild(children[children.length - 1])
   }
   domElem.appendChild(renderer.domElement)
-
-
-	const scene = new THREE.Scene()
-
-	const camera = new THREE.PerspectiveCamera(30, width / height, 0.01, 40)
+  
+  const camera = new THREE.PerspectiveCamera(30, width / height, 0.01, 40)
   camera.position.set(0, 0, 1)
-	scene.add(camera)
 
+  const scene = new THREE.Scene()
+  scene.add(camera)
+  
   const render = () => { renderer.render(scene, camera) }
-
-	const controls = new OrbitControls(camera, renderer.domElement)
-	controls.addEventListener('change', render)
-	controls.minDistance = 0.5
-	controls.maxDistance = 10
-
-	const loader = new PCDLoader()
-	loader.load('online.pcd', (points) => {
-		points.geometry.center()
-		points.geometry.rotateX(Math.PI)
-		scene.add(points)
-
-		render()
-	})
-
+  
+  const controls = new OrbitControls(camera, renderer.domElement)
+  controls.addEventListener('change', render)
+  controls.minDistance = 0.5
+  controls.maxDistance = 10
+  
+  const loader = new PCDLoader()
+  loader.load('online.pcd', (points) => {
+  	points.geometry.center()
+  	points.geometry.rotateX(Math.PI)
+  	scene.add(points)
+  
+  	render()
+  })
+  
   return {renderer, scene, camera}
 }
 
