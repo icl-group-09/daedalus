3b62a54f92f6ab6e9f1b82697c9af6d72454e64d
Woocashh <lj1019@ic.ac.uk>
2021-10-21 11:39:58 +0200

    Initial endpoint setup

--- api/app.py
+++ api/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/hw", methods=['GET', 'POST'])
 def hello_world():
     return {'hello': 'world'}
+
+
+@app.route("/files", methods=['GET'])
+def retrieve_file():
+   return request.json["file-name"]
+    
