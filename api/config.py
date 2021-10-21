0202ca726dcea64676a7af208153c98054bfab49
Woocashh <lj1019@ic.ac.uk>
2021-10-21 22:21:05 +0200

    Connect to Azure blob storage

---
+++ api/config.py
@@ -0,0 +1,3 @@
+AZURE_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=daedalusstorage;AccountKey=PDbDDznbsrhhbDFZcvfZ0BX489r7jFMClHOvUrm3hNKEknS5Kfd0CfwUFweBNcrJbdZ1N3G15eEvsF5AzCs12w==;EndpointSuffix=core.windows.net"
+BLOB_CONTAINER = "test-container"
+PCD_RELATIVE_DIRECTORY = "data/"
