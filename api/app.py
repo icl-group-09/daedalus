4243df5bad42a26b5bcd42883ae3af14c8c66416
egroge <egroge.stacey@gmail.com>
2021-12-20 14:53:58 +0000

    Try separating code again

--- api/app.py
+++ api/app.py
@@ -1,43 +1,5 @@
-import os
-
-from flask import Flask, Blueprint, send_file, render_template
-from flask.wrappers import Response
-import config
-from services.AzureService import AzureService
-from typing import Any
-from services.CloudStorageService import CloudStorageService
-
-bp = Blueprint("files", __name__, url_prefix="")
-
-@bp.route("/getPcd/<string:filename>", methods=["GET"])
-def get_pcd(filename: str) -> Response:
-
-    cloud_storage_service: CloudStorageService = AzureService()
-    download_file_path = os.path.join(
-        bp.root_path, config.PCD_RELATIVE_DIRECTORY, filename)
-
-    try:
-        # Retrieve the file from the cloud service and serve it as the response
-        cloud_storage_service.get_file(filename, download_file_path)
-        return send_file(download_file_path, as_attachment=True, download_name=filename)
-
-    except FileNotFoundError as err:
-        return Response(str(err), status=404)
-
-    finally:
-        # Remove file after sending attachment
-        try:
-            os.remove(download_file_path)
-        except FileNotFoundError:
-            pass
-
-
-@bp.route("/", methods=["GET"])
-def default() -> Any: # TODO fix
-
-    print("APP")
-    return render_template("index.html")
-
+from flask import Flask
+from . import files
 
 def create_app(test_config: str =None):
     # create and configure the app
@@ -52,7 +14,7 @@ def create_app(test_config: str =None):
 
     # apply the blueprints to the app
 
-    app.register_blueprint(bp)
+    app.register_blueprint(files.bp)
 
     return app
 
