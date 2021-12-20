6c9815e87eb2a1bd36c7115ec585c20620902358
Andrei Gramescu <36415459+andreigramescu@users.noreply.github.com>
2021-12-20 12:07:19 +0000

    [andrei, george] fix paths because of refact

--- api/__init__.py
+++ api/__init__.py
@@ -1,19 +1,19 @@
-from flask import Flask 
-import app
-
-def create_app(test_config: str =None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_pyfile(test_config)
-
-    # apply the blueprints to the app
-
-    app.register_blueprint(app.bp)
-
-    return app
+# from flask import Flask 
+# import app as _app
+# 
+# def create_app(test_config: str =None):
+#     # create and configure the app
+#     app = Flask(__name__, instance_relative_config=True)
+# 
+#     if test_config is None:
+#         # load the instance config, if it exists, when not testing
+#         app.config.from_pyfile('config.py', silent=True)
+#     else:
+#         # load the test config if passed in
+#         app.config.from_pyfile(test_config)
+# 
+#     # apply the blueprints to the app
+# 
+#     app.register_blueprint(_app.bp)
+# 
+#     return app
