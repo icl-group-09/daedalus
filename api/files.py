9c31b0b7a507150d7ff65333caa0654d33dc4184
egroge <egroge.stacey@gmail.com>
2021-12-20 15:03:23 +0000

    Add separated files

---
+++ api/files.py
@@ -0,0 +1,39 @@
+import os
+
+from flask import Blueprint, send_file, render_template
+from flask.wrappers import Response
+import config
+from typing import Any
+import services.AzureService as AzureService
+import services.CloudStorageService as CloudStorageService
+
+bp = Blueprint("files", __name__, url_prefix="")
+
+@bp.route("/getPcd/<string:filename>", methods=["GET"])
+def get_pcd(filename: str) -> Response:
+
+    cloud_storage_service: CloudStorageService = AzureService()
+    download_file_path = os.path.join(
+        bp.root_path, config.PCD_RELATIVE_DIRECTORY, filename)
+
+    try:
+        # Retrieve the file from the cloud service and serve it as the response
+        cloud_storage_service.get_file(filename, download_file_path)
+        return send_file(download_file_path, as_attachment=True, download_name=filename)
+
+    except FileNotFoundError as err:
+        return Response(str(err), status=404)
+
+    finally:
+        # Remove file after sending attachment
+        try:
+            os.remove(download_file_path)
+        except FileNotFoundError:
+            pass
+
+
+@bp.route("/", methods=["GET"])
+def default() -> Any: # TODO fix
+    return render_template("index.html")
+
+
