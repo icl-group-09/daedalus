d4b0a63755a89bbce89f5e7e2207c9b5b415bde9
egroge <egroge.stacey@gmail.com>
2021-12-21 16:26:19 +0000

    Change path issue with static content

--- api/flaskr/__init__.py
+++ api/flaskr/__init__.py
@@ -7,19 +7,18 @@ def create_app(test_config: str = None):
         __name__,
         instance_relative_config=True,
         static_url_path="",
-        static_folder="web/build",
-        template_folder="web/build",
+        static_folder="../web/build",
+        template_folder="../web/build",
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        flask_app.config.from_pyfile("config.py", silent=True)
+        flask_app.config.from_pyfile("../config.py", silent=True)
     else:
         # load the test config if passed in
         flask_app.config.from_pyfile(test_config)
 
     # apply the blueprints to the app
-
     flask_app.register_blueprint(files.bp)
 
     return flask_app
