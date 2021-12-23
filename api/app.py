c55ecc1c30986c92dfd9990a710d0d05b1966a94
Andrei Gramescu <36415459+andreigramescu@users.noreply.github.com>
2021-12-23 09:08:03 +0000

    refresh gltfs dir on backend startup

--- api/app.py
+++ api/app.py
@@ -1,6 +1,15 @@
 from flask import render_template
 from typing import Any
 from flaskr import create_app
+import os, shutil
+
+gltfs_path = os.path.join(os.getcwd(), "gltfs")
+try:
+    shutil.rmtree(gltfs_path)
+    os.mkdir(gltfs_path)
+except OSError as error:
+    print(error)
+    print("Something went wrong while trying to refresh the gltfs dir")
 
 app = create_app()
 
