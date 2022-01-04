479ce51d98177347a07556deac8b639f9aa5cd7e
TheAdamG <adam.gould39@gmail.com>
2022-01-04 15:08:50 +0000

    Fix pixels with 0 alpah being rendered in pcd and fix pcd orientation when generated

--- api/pcd_generator/pointcloud.py
+++ api/pcd_generator/pointcloud.py
@@ -1,6 +1,7 @@
 import matplotlib.image as mpimg
 import numpy as np
 import os
+import sys
 
 # Read in the image and depth map as seperate assets
 def generate_pcd(image_location: str, depth_map_location: str, output_name: str) -> str:
@@ -12,7 +13,11 @@ def generate_pcd(image_location: str, depth_map_location: str, output_name: str)
     depth_map = mpimg.imread(depth_map_location)
     image = mpimg.imread(image_location)
 
-    rows, cols, _ = depth_map.shape
+    if len(depth_map.shape) == 3:
+      rows, cols, _ = depth_map.shape
+    else:
+      rows, cols = depth_map.shape
+
 
     # Cut down pixels for time purpose
     # Computations need to be under 30s
@@ -40,10 +45,9 @@ def generate_pcd(image_location: str, depth_map_location: str, output_name: str)
         for x in range(0, cols, pixel_cut):
             for y in range(0, rows, pixel_cut):
                 height_sum = (depth_map[y, x, 0] + depth_map[y, x, 1] + depth_map[y, x, 2])
-                if height_sum > 200:
-                    height_sum = 0
                 depth_values[x][y] = height_sum
 
+        checkAlpha = len(image.shape) == 3 and image.shape[2] == 4
         a = 0
         b = 0.25
         num = (depth_values - np.min(depth_values)) * (b - a)
@@ -51,9 +55,16 @@ def generate_pcd(image_location: str, depth_map_location: str, output_name: str)
         depth_values = a + (num / den)
         for x in range(0, cols, pixel_cut):
             for y in range(0, rows, pixel_cut):
-                hex_color = image[y, x, 0] * \
+              if checkAlpha:
+                if image[y, x, 3] == 0:
+                  continue
+              hex_color = image[y, x, 0] * \
                     (16 ** 4) + image[y, x, 1] * (16 ** 2) + image[y, x, 2]
-                pcd.write(str(x/1000) + " " + str(depth_values[x][y]) + " " +
-                          str(y/1000) + " " + str(hex_color) + "\n")
+              pcd.write(str(x/1000) + " " + str(-depth_values[x][y]) + " " +
+                          str(-y/1000) + " " + str(hex_color) + "\n")
 
     return path
+
+  
+if __name__ == "__main__":
+  generate_pcd(sys.argv[1], sys.argv[2], "test")
