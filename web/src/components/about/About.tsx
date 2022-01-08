0807e27f9d87c8e87958adad1ca08728f123f485
TheAdamG <adam.gould39@gmail.com>
2022-01-08 18:44:06 +0000

    Add about page text

--- web/src/components/about/About.tsx
+++ web/src/components/about/About.tsx
@@ -15,7 +15,11 @@ export function About({show, setShowAbout}: AboutProps) {
           <h3> About </h3>
         </Modal.Header>
         <Modal.Body>
-          TODO: WRITE ABOUT PAGE
+       <p> 
+         Daedalus is a drone data visualisation tool aimed at simplifying how users can interact with the data they collect. Users simply upload the image of the terrain they are surveying and a corresponding height map and a 3D model will be visualised for their use. They will be able to see in detail how the terrain is shaped and be able to manipulate the model, for example by zooming into the smallest details. Drone data is much more intuitive and useful when viewed like this and prevents having to trawl through raw 2D images. 
+          <br/> <br/>
+          Daedalus offers a variety of tools: a height map visualisation layer, changing the resolution of the model to spot intricate details and the ability to visualise the terrain in Augmented Reality to gain a better insight into how the data reflects the real world. With a modern and portable design, Daedalus works anywhere, all that's needed is a browser. The mobile interface makes working with Daedalus in the field even easier
+        </p>
         </Modal.Body>
 			<Modal.Footer>
 				<Button variant="secondary" onClick={() => setShowAbout(false)}>
